06141dfcadfec56c02b80ff489b0b067a61708b9
varnikarangaraj <varni014@gmail.com>
2016-07-14 14:43:57 +0530

    Create public.c

---
+++ public.c
@@ -0,0 +1,19 @@
+ 
+#include <stdio.h>
+ 
+int main()
+{
+    char ch;
+    printf("Enter any character: ");
+    scanf("%c", &ch);
+    if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+    {
+        printf("Character is an ALPHABET.\n");
+    }
+    else
+    {
+        printf("Character is NOT ALPHABET.\n");
+    }
+ 
+    return 0;
+} 
